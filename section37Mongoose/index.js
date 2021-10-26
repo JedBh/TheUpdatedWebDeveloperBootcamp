c1ea32d4d2bde4daf5f54b5c1ae58aa9b2e3eecb
jbenhod <jbenhod@gmail.com>
2021-10-26 13:28:07 +0300

    Things are up to date with my PC

--- section37Mongoose/index.js
+++ section37Mongoose/index.js
@@ -10,6 +10,7 @@ const productSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
+    maxlength: 20,
   },
   price: {
     type: Number,
@@ -26,27 +27,57 @@ const productSchema = new mongoose.Schema({
     online: {
       type: Number,
       default: 0,
+      min: [0, "Quantity must be over than 0"],
     },
     inStore: {
       type: Number,
       default: 0,
+      min: [0, "Quantity must be over than 0"],
     },
+  },
+  size: {
+    type: String,
+    enum: ["S", "M", "L"],
   },
 });
 
+productSchema.methods.toggleOnSale = function () {
+  this.onSale = !this.onSale;
+  return this.save();
+};
+
+productSchema.methods.greet = function () {
+  console.log("Hello! Hi! Howdy!");
+  console.log("- from ", this.name);
+};
+
+productSchema.methods.addCatagory = function (newCat) {
+  this.catagories.push(newCat);
+  return this.save;
+};
+
 const Product = mongoose.model("Product", productSchema);
 
-const bike = new Product({
-  name: "Mountain Bike",
-  price: 599,
-  catagories: ["bike", "offroad"],
-  qty: { online: 9 },
-})
-  .save()
-  .then((data) => {
-    console.log("It Worked!");
-    console.log(data);
-  })
-  .catch((err) => {
-    console.log("It failed!", err);
-  });
+// const bike = new Product({
+//   name: "Mountain Bike",
+//   price: 599,
+//   catagories: ["bike", "offroad"],
+//   qty: { online: 9 },
+// })
+//   .save()
+//   .then((data) => {
+//     console.log("It Worked!");
+//     console.log(data);
+//   })
+//   .catch((err) => {
+//     console.log("It failed!", err);
+//   });
+
+const findProduct = async () => {
+  const foundProduct = await Product.findOne({ name: "Mountain Bike" });
+  console.log(foundProduct);
+  await foundProduct.toggleOnSale();
+  console.log(foundProduct);
+};
+
+findProduct();
