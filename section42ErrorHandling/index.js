03531659bd2895ab8697bee720425a1a713f8109
jbenhod <jbenhod@gmail.com>
2021-10-31 21:04:12 +0200

    our custom error class

--- section42ErrorHandling/index.js
+++ section42ErrorHandling/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
+const appError = require("./appError");
 
 // app.use(morgan("dev"));
 
@@ -20,7 +21,7 @@ const verifyPassword = (req, res, next) => {
   if (password === "chickennugget") {
     next();
   } else {
-    throw new Error("Password required");
+    throw new appError("password required", 401);
   }
 };
 
@@ -42,12 +43,18 @@ app.get("/secret", verifyPassword, (req, res) => {
   res.send("Sometimes I wear headphones so I don't have to talk to anyone!");
 });
 
+app.get("/admin", (req, res) => {
+  throw new appError("You are not an admin!", 403);
+});
+
 app.use((req, res) => {
   res.status(404).send("NOT FOUND!");
 });
 
 app.use((err, req, res, next) => {
-  console.log(err);
+  const { status = 500 } = err;
+  const { message = "Error:" } = err;
+  res.status(status).send(message);
   next(err);
 });
 
