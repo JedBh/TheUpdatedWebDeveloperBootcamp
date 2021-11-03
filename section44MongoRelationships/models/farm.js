be69b9de1d520f1808d5972b816467b397704f7c
unknown <jbenhod@gmail.com>
2021-11-03 16:24:32 +0200

    one to many

---
+++ section44MongoRelationships/models/farm.js
@@ -0,0 +1,70 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+// connecting to mongodb
+main()
+  .then(() => {
+    console.log("Mongo Connected");
+  })
+  .catch((err) => console.log("Mongo Connection Error", err));
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/relationshipDemo");
+}
+
+const productSchema = Schema({
+  name: String,
+  price: Number,
+  season: {
+    type: String,
+    enum: ["spring", "summer", "fall", "winter"],
+  },
+});
+
+const farmSchema = Schema({
+  name: String,
+  city: String,
+  products: [{ type: Schema.Types.ObjectId, ref: "Product" }],
+});
+
+const Product = mongoose.model("Product", productSchema);
+const Farm = mongoose.model("Farm", farmSchema);
+
+// const makeFarm = async () => {
+//   const farm = new Farm({
+//     name: "Full Belly Farms",
+//     city: "Guinda, CA",
+//   });
+//   const melon = await Product.findOne({ name: "Goddes Melon" });
+//   farm.products.push(melon);
+//   await farm.save();
+//   console.log(farm);
+// };
+
+// makeFarm();
+
+// Product.insertMany([
+//   {
+//     name: "Goddes Melon",
+//     price: 4.99,
+//     season: "summer",
+//   },
+//   {
+//     name: "Sugar Baby Watermelon",
+//     price: 5.99,
+//     season: "summer",
+//   },
+//   {
+//     name: "Asparagus",
+//     price: 3.99,
+//     season: "spring",
+//   },
+// ]);
+
+const addProduct = async () => {
+  const farm = await Farm.findOne({ name: "Full Belly Farms" });
+  const watermelon = await Product.findOne({ name: "Sugar Baby Watermelon" });
+  farm.products.push(watermelon);
+  farm.save();
+};
+
+addProduct();
---
+++ section44MongoRelationships/models/user.js
@@ -0,0 +1,57 @@
+const mongoose = require("mongoose");
+
+// connecting to mongodb
+main()
+  .then(() => {
+    console.log("Mongo Connected");
+  })
+  .catch((err) => console.log("Mongo Connection Error", err));
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/relationshipDemo");
+}
+
+const userSchema = new mongoose.Schema({
+  first: String,
+  last: String,
+  addresses: [
+    {
+      _id: { id: false },
+      street: String,
+      city: String,
+      state: String,
+      country: String,
+    },
+  ],
+});
+
+const User = mongoose.model("User", userSchema);
+
+const makeUser = async () => {
+  const u = new User({
+    first: "Harry",
+    last: "Potter",
+  });
+  u.addresses.push({
+    street: "123 Sesame St.",
+    city: "New York",
+    state: "New York",
+    country: "USA",
+  });
+  const res = await u.save();
+  console.log(res);
+};
+
+const addAddress = async (id) => {
+  const user = await User.findById(id);
+  user.addresses.push({
+    street: "99 3rd St.",
+    city: "New York",
+    state: "New York",
+    country: "USA",
+  });
+  const res = await user.save();
+  console.log(res);
+};
+
+addAddress("61828f2552c7bab04a27a6bc");
