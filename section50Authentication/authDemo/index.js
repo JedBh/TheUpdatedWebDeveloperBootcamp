161e26560338ef92d310f9a6c05c850a2099f7bc
JedBH <jbenhod@gmail.com>
2021-11-16 20:17:12 +0200

    auth demo: register

--- section50Authentication/authDemo/index.js
+++ section50Authentication/authDemo/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const bcrypt = require("bcrypt");
 
 const User = require("./models/user");
 
@@ -11,19 +12,33 @@ app.use(express.urlencoded({ extended: true }));
 
 // connecting to mongodb
 
-// main()
-//   .then(() => {
-//     console.log("Mongo Connected");
-//   })
-//   .catch((err) => console.log("Mongo Connection Error", err));
+main()
+  .then(() => {
+    console.log("Mongo Connected");
+  })
+  .catch((err) => console.log("Mongo Connection Error", err));
 
-// async function main() {
-//   await mongoose.connect("mongodb://localhost:27017/");
-// }
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/authDemo");
+}
 
 // routes
+app.get("/", (req, res) => {
+  res.send("This is the homepage");
+});
 app.get("/register", (req, res) => {
   res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+
+  //   await user.save();
+  //   res.redirect("/secret");
+  const hash = await bcrypt.hash(password, 12);
+  const user = new User({ username, password: hash });
+  await user.save();
+  res.redirect("/");
 });
 
 app.get("/secret", (req, res) => {
