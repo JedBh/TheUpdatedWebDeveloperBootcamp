f7f9d275f3e5f355cf2e5e1f7c8ce54d56bf8a39
JedBh <jbenhod@gmail.com>
2021-11-17 15:45:40 +0200

    auth demo: require login middleware

--- section50Authentication/authDemo/index.js
+++ section50Authentication/authDemo/index.js
@@ -7,6 +7,18 @@ const session = require("express-session");
 
 const User = require("./models/user");
 
+// connecting to mongodb
+
+main()
+  .then(() => {
+    console.log("Mongo Connected");
+  })
+  .catch((err) => console.log("Mongo Connection Error", err));
+
+async function main() {
+  await mongoose.connect("mongodb://localhost:27017/authDemo");
+}
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
@@ -19,17 +31,10 @@ const sessionOptions = {
 
 app.use(session(sessionOptions));
 
-// connecting to mongodb
-
-main()
-  .then(() => {
-    console.log("Mongo Connected");
-  })
-  .catch((err) => console.log("Mongo Connection Error", err));
-
-async function main() {
-  await mongoose.connect("mongodb://localhost:27017/authDemo");
-}
+const requireLogin = (req, res, next) => {
+  if (!req.session.user_id) return res.redirect("/login");
+  next();
+};
 
 // routes
 app.get("/", (req, res) => {
@@ -70,9 +75,13 @@ app.post("/logout", (req, res) => {
   res.redirect("/login");
 });
 
-app.get("/secret", (req, res) => {
+app.get("/secret", requireLogin, (req, res) => {
   if (!req.session.user_id) res.redirect("/login");
   res.render("secret");
+});
+
+app.get("/topsecret", requireLogin, (req, res) => {
+  res.send("TOP SECRET!!!");
 });
 
 const PORT = 3000;
